95a26086febf007f5e9c4e8209df082f0c8bdc08
DamienA <alves.d24@laposte.net>
2025-06-05 15:14:46 +0200

    feat(DetailsViewModel): recover reviews by view model

--- app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/DetailsViewModel.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/DetailsViewModel.java
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel;
 import com.openclassrooms.tajmahal.R;
 import com.openclassrooms.tajmahal.data.repository.RestaurantRepository;
 import com.openclassrooms.tajmahal.domain.model.Restaurant;
+import com.openclassrooms.tajmahal.domain.model.Review;
 
 import javax.inject.Inject;
 
 import java.util.Calendar;
+import java.util.List;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
 
@@ -82,6 +84,16 @@ public class DetailsViewModel extends ViewModel {
                 dayString = "";
         }
         return dayString;
+    }
+
+    /**
+     * Fetches the reviews of the Taj Mahal restaurant.
+     *
+     * @return LiveData object containing the list of reviews of the Taj Mahal restaurant.
+     *
+     */
+    public LiveData<List<Review>> getReviews() {
+        return restaurantRepository.getReviews();
     }
 
 }
